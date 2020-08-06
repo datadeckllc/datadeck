0e8b35fe07c067043680e4f0712e05571770c09f
Jorge Calderon <jcald1@gmail.com>
2020-08-06 01:29:29 -0400

    Add status message to the page

--- components/dragger.js
+++ components/dragger.js
@@ -11,10 +11,13 @@ const { Dragger } = Upload;
 const MESSAGE_SUCCESS_DURATION = 5;
 const MESSAGE_FAILURE_DURATION = 15;
 
+const UPLOAD_MESSAGE_UPLOAD = 'UPLOAD FILE';
+const UPLOAD_MESSAGE_PROCESSING = 'PROCESSING FILE...';
+
 const EXCEL_MIME_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
 
 export default () => {
-    const [isUploading, setIsUploading] = useState(false);
+    const [uploadMessage, setUploadMessage] = useState(UPLOAD_MESSAGE_UPLOAD);
 
     const props = {
         accept: EXCEL_MIME_TYPE,
@@ -27,17 +30,18 @@ export default () => {
             const { status } = info.file;
             if (status !== 'uploading') {
                 console.log(info.file, info.fileList);
-                setIsUploading(false);
+                setUploadMessage(UPLOAD_MESSAGE_PROCESSING);
             }
             else {
-                setIsUploading(true);
+                setUploadMessage(UPLOAD_MESSAGE_UPLOAD);
             }
             if (status === 'done') {
                 const spreadsheetRes = await axios.post(`${process.env.apiClient.url}/get-spreadsheet`, info.file.response,{ responseType: 'arraybuffer', headers: { 'content-type': 'application/json' } } )
                     .catch(err => {
-                        const errMsg = `Processing of ${info.file.name} generated an error.  Error message: ${err.message}.`;
+                        const errMsg = `FAILURE: Processing of ${info.file.name} generated an error.  Error message: ${err.message}.`;
                         console.error(errMsg, err);
                         message.error(errMsg, MESSAGE_FAILURE_DURATION);
+                        setUploadMessage(errMsg);
                     });
                 if (!spreadsheetRes) {
                     return;
@@ -46,11 +50,14 @@ export default () => {
                 var spreadsheetName = contentDisposition.parse(spreadsheetRes.headers['content-disposition']).parameters.filename;
 
                 if (spreadsheetRes.status === 206) {
-                    const errMsg = `Processing of ${info.file.name} generated an error, but a spreadsheet with potential partial results was created.  Spreadsheet will download with partial results.`;
+                    const errMsg = `PARTIAL SUCCESS: Processing of ${info.file.name} generated an error, but a spreadsheet with potential partial results was created.  Spreadsheet will download with partial results.`;
                     console.error(errMsg);
                     message.error(errMsg, MESSAGE_FAILURE_DURATION);
+                    setUploadMessage(errMsg);
                 }else {
-                    message.success(`${info.file.name} file uploaded and processed successfully.  Spreadsheet will download.`, MESSAGE_SUCCESS_DURATION);
+                    const msg = `SUCCESS: ${info.file.name} file uploaded and processed successfully.  Spreadsheet will download.`;
+                    message.success(msg, MESSAGE_SUCCESS_DURATION);
+                    setUploadMessage(msg);
                 }
 
                 const blob = new Blob([spreadsheetRes.data], {type: `${EXCEL_MIME_TYPE};charset=utf-8`});
@@ -58,16 +65,17 @@ export default () => {
 
             } else if (status === 'error') {
                 const details = info.file && info.file.response && info.file.response.msg;
-                const errMsg = `${info.file.name} file upload failed. ${details ? details : ''}`;
+                const errMsg = `FAILED:${info.file.name} file upload failed. ${details ? details : ''}`;
                 console.error(errMsg);
                 message.error(errMsg, MESSAGE_FAILURE_DURATION);
+                setUploadMessage(errMsg)
             }
         },
     };
 
    return (
        <div>
-            <div className={`${utilStyles.headingMd} ${utilStyles.red} ${utilStyles.center}`}>{isUploading ? "PROCESSING FILE..." : 'Upload File'}</div>
+            <div className={`${utilStyles.headingMd} ${utilStyles.red} ${utilStyles.center}`}>{uploadMessage}</div>
             <Dragger {...props}>
 
                 <p className="ant-upload-drag-icon">
